580592efee964d66c1b7dd6b04a831e843e3eb71
Lorcan Mc <lorcan.jmc@gmail.com>
2022-06-21 15:48:14 +0100

    recieving xml file from the elexon live data server

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+  const [xml, setXml] = useState(null)
+  if(xml) {
+    console.log(xml.getElementsByTagName("FUEL")[0])
+  }
+
+  useEffect(() => {
+    async function loadData() {
+      const res = await fetch(`https://lorcancorsproxy.herokuapp.com/https://downloads.elexonportal.co.uk/fuel/download/latest?key=2eo5rjhgzwn4mm6`)
+      console.log(res)
+      const data = await res.text()
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(data,"text/xml");
+      setXml(xmlDoc)
+      console.log(xmlDoc)
+    }
+    loadData()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+			Parse XML using ReactJS
+      {xml && xml.getElementsByTagName("FUEL")[0].getAttribute("TYPE")}
+			{/* {xml &&
+        xml.getElementsByTagName("FUEL").map((item) => {
+				return (
+					<span>{item.TYPE}</span>
+				)
+			})
+		} */}
+		</div>
     </div>
   );
 }
