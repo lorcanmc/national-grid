44f77500e7e0f6a1e67371affaea006a5e97d58d
Lorcan Mc <lorcan.jmc@gmail.com>
2022-06-22 22:23:31 +0100

    added pie chart

--- src/App.js
+++ src/App.js
@@ -1,32 +1,43 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 
+import { PieChart } from "react-minimal-pie-chart";
+import { XMLParser } from "fast-xml-parser";
+
+const defaultLabelStyle = {
+  fill: "white",
+  fontSize: "5px",
+  fontWeight: "bold",
+  fontFamily: "sans-serif",
+};
+
 function App() {
-  const [xml, setXml] = useState(null);
-  if (xml) {
-    console.log(xml);
-  }
+  const [halfHourJson, setHalfHourJson] = useState(null);
 
   useEffect(() => {
     async function loadData() {
-      const solarRes = await fetch(
-        `https://api0.solar.sheffield.ac.uk/pvlive/v3/`
-      );
-      const solarData = await solarRes.json();
-      console.log(solarData);
+      // sheffield solar
+      // const solarRes = await fetch(
+      //   `https://api0.solar.sheffield.ac.uk/pvlive/v3/`
+      // );
+      // const solarData = await solarRes.json();
+      // console.log(solarData);
 
+      //Elexon
       const res = await fetch(
         `https://lorcancorsproxy.herokuapp.com/https://downloads.elexonportal.co.uk/fuel/download/latest?key=2eo5rjhgzwn4mm6`
       );
       const data = await res.text();
-      const parser = new DOMParser();
-      const xmlDoc = parser.parseFromString(data, "text/xml");
-      setXml([
-        ...xmlDoc
-          .getElementsByTagName("LAST24H")[0]
-          .getElementsByTagName("FUEL"),
-      ]);
-      console.log(xmlDoc);
+      const options = {
+        ignoreAttributes: false,
+      };
+      const parser = new XMLParser(options);
+      let elexonJsonObj = parser.parse(data);
+      console.log(elexonJsonObj);
+      let lastHalfHour = elexonJsonObj.GENERATION_BY_FUEL_TYPE_TABLE.HH.FUEL;
+      console.log(lastHalfHour);
+
+      setHalfHourJson(lastHalfHour);
     }
     loadData();
   }, []);
@@ -34,22 +45,44 @@ function App() {
   return (
     <div className="App">
       <div>
-        <h1>Current Grid Data</h1>
-        {xml &&
-          xml.map((fuel) => {
-            return (
-              <div>
-                <span>
-                  <b>{fuel.getAttribute("TYPE")}: </b>
-                </span>
-                <span>{fuel.getAttribute("VAL")} MW</span>
-              </div>
-            );
-          })}
-        {/* {xml &&
-          xml.getElementsByTagName("FUEL").map((item) => {
-            return <span>{item.getAttribute("TYPE")}</span>;
-          })} */}
+        <h1>Current UK Grid Data</h1>
+        {halfHourJson && (
+          <div className="pie-container">
+            <PieChart
+              data={[
+                {
+                  title: "Natural Gas",
+                  value: Number(halfHourJson[0]["@_VAL"]),
+                  color: "rgb(194, 53, 150)",
+                },
+                {
+                  title: "Nuclear",
+                  value: Number(halfHourJson[4]["@_VAL"]),
+                  color: "rgb(76, 174, 55)",
+                },
+                // { title: "Three", value: 20, color: "#6A2135" },
+              ]}
+              label={({ dataEntry }) => dataEntry.title}
+              labelStyle={{
+                ...defaultLabelStyle,
+              }}
+              labelPosition={60}
+            />
+          </div>
+        )}
+        <div className="values-container">
+          {halfHourJson &&
+            halfHourJson.map((fuel, i) => {
+              return (
+                <div key={i}>
+                  <span>
+                    <b>{fuel["@_TYPE"]}: </b>
+                  </span>
+                  <span>{Math.round(fuel["@_VAL"] / 10) / 100} GW</span>
+                </div>
+              );
+            })}
+        </div>
       </div>
     </div>
   );
